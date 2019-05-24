82103faa7df03567db6a15f8510546e7594a0646
zigmon <zigmon@pacbell.net>
2019-05-23 23:08:28 -0700

    Assignment jams routing: completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <main>
         <Route exact path="/" component={Landing} />
         <Route path="/library" component={Library} />
+        <Route path="/album" component={Album} />
       </main>
     </div>
   );
