704b4a118bf70d53dee930964a83097809352141
zigmon <zigmon@pacbell.net>
2019-06-05 22:38:57 -0700

    Assignment Player Bar Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,7 +54,16 @@ class Album extends Component {
             const newIndex = Math.max(0, currentIndex - 1);
             const newSong = this.state.album.songs[newIndex];
             this.setSong(newSong);
-            this.play();}
+            this.play();
+        }
+
+        handleNextClick() {
+            const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+            const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+            const newSong = this.state.album.songs[newIndex];
+            this.setSong(newSong);
+            this.play();
+        }
 
 
 
@@ -105,6 +114,7 @@ class Album extends Component {
                     currentSong={this.state.currentSong}
                     handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                     handlePrevClick={() => this.handlePrevClick()}
+                    handleNextClick={() => this.handleNextClick()}
                 />
             </section>
         );
